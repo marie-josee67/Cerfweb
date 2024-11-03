986612edd54778e2169a4823b757ce5627d0d1f0
marie-josee <mariejosee89@hotmail.fr>
2024-11-03 12:51:19 +0100

    mise en forme formulaire et localisation.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// bouton dark /light mode
 function boutonDarkMode() {
     document.body.classList.toggle('dark');
     
-    const isDarkMode = document.body.classList.contains('dark');
+    const ISDARKMODE = document.body.classList.contains('dark');
 
     // Changement des images du mode
-    document.getElementById('mode-icon').className = isDarkMode ? 'fa-solid fa-sun' : 'fa-solid fa-moon';
+    document.getElementById('mode-icon').className = ISDARKMODE ? 'fa-solid fa-sun' : 'fa-solid fa-moon';
 
     // rejouter autant que d'iamage par page a changer
-    document.getElementById('image1').src = isDarkMode ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
-    document.getElementById('image2').src = isDarkMode ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
-    document.getElementById('image3').src = isDarkMode ? 'dark-image3.jpg' : 'light-image3.jpg';
+    document.getElementById('image1').src = ISDARKMODE ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
+    document.getElementById('image2').src = ISDARKMODE ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
+    document.getElementById('image3').src = ISDARKMODE ? 'dark-image3.jpg' : 'light-image3.jpg';
+}
+
+// bouton numéro
+function magieNumero() {
+    // Remplacer le texte du bouton par le numéro de téléphone
+    document.getElementById("telephoneBoutton").innerText = "+33 1 23 45 67 89";
 }
