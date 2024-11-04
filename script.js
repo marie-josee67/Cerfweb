d4d2b720724c78dff5c52c2a74162d9c84b60099
marie-josee <mariejosee89@hotmail.fr>
2024-11-04 20:32:42 +0100

    mise en form de l'accueil.

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function boutonDarkMode() {
     // Changement des images du mode
     document.getElementById('mode-icon').className = ISDARKMODE ? 'fa-solid fa-sun' : 'fa-solid fa-moon';
 
-    // rejouter autant que d'iamage par page a changer
+    // rejouter autant que d'image par page a changer
     document.getElementById('logoNavBarre').src = ISDARKMODE ? './img/logo/Fichier 8.png' : './img/logo/Fichier 9.png';
     document.getElementById('image2').src = ISDARKMODE ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
+    document.getElementById('service').src = ISDARKMODE ? './img/logo/fichier 2.png' : './img/logo/Fichier 3.png';
     document.getElementById('image3').src = ISDARKMODE ? 'dark-image3.jpg' : 'light-image3.jpg';
+
 }
 
 // bouton numéro page contact
